a47d098004d2fdacc68b7081c7ac6366f2410788
ExtremeXT <extremext@lineageos.org>
2026-06-19 11:37:02 +0300

    a55x-common: Build AIDL SGR Gralloc

--- common.mk
+++ common.mk
@@ -123,6 +123,12 @@ PRODUCT_PACKAGES += android.hardware.vibrator-service.s5e8845
 $(call soong_config_set_bool,samsungVibratorVars,duration_amplitude,true)
 
 # Graphics
+
+PRODUCT_PACKAGES += \
+    android.hardware.composer.hwc3-service.slsi \
+    android.hardware.graphics.allocator-service-sgr \
+    android.hardware.graphics.mapper@4.0-impl-sgr
+
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 450dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
@@ -310,3 +316,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
+# Call Samsung LSI board support package makefiles
+$(call inherit-product, hardware/samsung_slsi-linaro/graphics/base/hwcomposer_property.mk)
+$(call inherit-product, hardware/samsung_slsi-linaro/config/config.mk)
